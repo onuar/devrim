a2a3c9dc040498fc63837eb6a8ae1c568c12eca3
Onur Aykac <onuraykac@gmail.com>
2017-09-03 02:00:48 +0800

    added sample-app delaying

--- sample-app/routes/index.js
+++ sample-app/routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  
-  setTimeout(function() {
-    res.render('index', { title: 'Express' });  
-  }, 0);
-  
+router.get('/', function (req, res, next) {
+  var waitingTime = Math.floor(Math.random() * 5) + 1;
+  setTimeout(function () {
+    res.render('index', { message: 'Sleeping time: ' + waitingTime });
+  }, waitingTime * 1000);
+
 });
 
 module.exports = router;
